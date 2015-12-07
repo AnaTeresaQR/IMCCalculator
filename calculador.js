b35cb0c9fbb1aa5f75ebc169be5bea933ddfdc85
Juan Miguel <juan.ariasmejias@ucr.ac.cr>
2015-12-06 22:51:37 -0600

    Visualizacion de estados
    Se muestran los estados de los pacientes

--- calculador.js
+++ calculador.js
@@ -16,12 +16,12 @@ $(document).ready(function(){
     talla = parseFloat(talla);
 
     // Calcula el IMC
-    var mostrar = calcularIMC(peso, talla);
+    var imc = calcularIMC(peso, talla);
 
     // Restringe a solo dos decimales
-    mostrar = mostrar.toFixed(2);
+    imc = imc.toFixed(2);
     // Mostramos la respuesta
-    respuesta.text("Tu IMC es de: " + mostrar);
+    respuesta.text("Tu IMC es de: " + imc + " un estado: " + clasificar(imc));
   });
 
 });
@@ -34,4 +34,39 @@ $(document).ready(function(){
 function calcularIMC(peso, talla) {
   var resultado = peso / (talla * talla);
   return resultado;
+}
+
+/**
+ * Clasifica el resultado
+ * @param imc se clasifica segun los rangos
+ */
+function clasificar(imc) {
+
+  if (imc <= 18.5) {
+    return "Bajo Peso";
+  }
+
+  if (imc > 18.5 && imc <= 24.9) {
+    return "Normal";
+  }
+
+  if (imc > 24.9 && imc <= 29.9) {
+    return "Sobrepeso";
+  }
+
+  if (imc > 29.9 && imc <= 34.9) {
+    return "Obesidad I";
+  }
+
+  if (imc > 34.9 && imc <= 39.9) {
+    return "Obesidad II";
+  }
+
+  if (imc > 39.9 && imc <= 49.9) {
+    return "Obesidad Mórbida";
+  }
+
+  if (imc > 49.9) {
+    return "Súper Obesidad";
+  }
 }
